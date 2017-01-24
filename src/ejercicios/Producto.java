102e30becb499053754b37824b1d8d89c52e676c
Manuel Molino <manolo@iesvirgendelcarmen.com>
2017-01-24 12:48:30 +0100

    Ejercicio de composición

---
+++ src/ejercicios/Producto.java
@@ -0,0 +1,40 @@
+package ejercicios;
+
+public class Producto {
+	private int identificador;
+	private String nombreProducto;
+	private double precioProducto;
+	
+	public Producto(int identificador, String nombreProducto, double precioProducto) {
+		this.identificador = identificador;
+		this.nombreProducto = nombreProducto;
+		this.precioProducto = precioProducto;
+	}
+	public int getIdentificador() {
+		return identificador;
+	}
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+	public double getPrecioProducto() {
+		return precioProducto;
+	}
+	@Override
+	public String toString() {
+		return "Producto [identificador=" + identificador + ", nombreProducto=" + nombreProducto + ", precioProducto="
+				+ precioProducto + "]";
+	}
+	//creamos un método main para probar la clase
+	/*public static void main(String[] args) {
+		Producto p = new Producto(1, "producto ejemplo", 100.23);
+		System.out.println(p.identificador);
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/ejercicios/Tienda.java
@@ -0,0 +1,50 @@
+package ejercicios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tienda {
+	private List<Producto> listaProductos;
+	
+	//constructor genera una lista vacía
+	public Tienda() {
+		this.listaProductos = new ArrayList<Producto>();
+	}
+	public boolean insertarProducto(Producto p){
+		return listaProductos.add(p);
+	}
+	public boolean eliminarProducto(Producto p){
+		return listaProductos.remove(p);
+	}
+	public List<Producto> getListaProductos() {
+		return listaProductos;
+	}
+	public Producto obtenerProductoMasCaro(){
+		Producto productoMasCaro = 
+				listaProductos.get(0);
+		for (Producto referenciaProductoLista : listaProductos) {
+			if (referenciaProductoLista.getPrecioProducto() > 
+				productoMasCaro.getPrecioProducto())
+				productoMasCaro = referenciaProductoLista;
+		}
+		return productoMasCaro;
+	}
+	public Producto obtenerProductoMasBarato(){
+		Producto productoMasBarato = 
+				listaProductos.get(0);
+		for (Producto referenciaProductoLista : listaProductos) {
+			if (referenciaProductoLista.getPrecioProducto() > 
+				productoMasBarato.getPrecioProducto())
+				productoMasBarato = referenciaProductoLista;
+		}
+		return productoMasBarato;
+	}
+	@Override
+	public String toString() {
+		return "Tienda [listaProductos=" + listaProductos + "]";
+	}
+	
+	
+	
+	
+}
